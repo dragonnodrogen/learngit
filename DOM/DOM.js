500c1a10931169c0d049d9021132b5181a68688a
dragonnodrogen <37856483+dragonnodrogen@users.noreply.github.com>
2018-05-07 11:40:30 +0800

    DOM

---
+++ DOM/DOM.js
@@ -0,0 +1,12 @@
+function init(){
+  var h1obj;
+  h1obj = document.getElementById('title');
+  alert(h1obj.innerHTML);
+  h1obj.innerHTML = 'learn js again';
+  h1obj.setAttribute('class','tit');
+}
+function changepic(){
+  var picobj = document.getElementById('pic');
+  picobj.
+}
+window.onload = init;
