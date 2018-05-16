8fdf7df19614f815925d82b839741f567e346bbf
dragonnodrogen <37856483+dragonnodrogen@users.noreply.github.com>
2018-05-16 15:00:48 +0800

    shipgamever1.1

--- battleship.js
+++ battleship.js
@@ -1,27 +1,21 @@
-var location1=3;
-var location2=4;
-var location3=5;
-var guess;
-var hits=0;
-var guesses=0;
-var isSunk=false;
-while(isSunk==false){
-    guess=prompt('Ready,aim,fire!(enter a number from 0-6):');
-    if(guess<0||guess>6){
-        alert('Please enter a valid cell number!');
-    }   else{
-        guesses=guesses+1;
-        if(guess==location1||guess==location2||guess==location3){
-            alert('HIT!')
-            hits=hits+1;
-            if(hits==3){
-                isSunk=true;
-                alert('you sank my battleship!');
-            }
-        }   else{
-            alert('MISS')
-        }
-    }
+var view = {
+  displayMessage:function(msg){
+    var messageArea = document.getElementById('messageArea');
+    messageArea.innerHTML = msg;
+  },
+  displayHit:function(location){
+    var cell = document.getElementById(location);
+    cell.setAttribute('class', 'hit');
+  },
+  displayMiss:function(location){
+    var cell = document.getElementById(location);
+    cell.setAttribute('class', 'miss');
+  }
 }
-var stats='You took ' + guesses + ' guesses to sink the battleship,' + 'which means your shooting accuracy was' + (3/guesses);
-alert(stats);
+view.displayMiss('00');
+view.displayHit('34');
+view.displayMiss('55');
+view.displayHit('12');
+view.displayMiss('25');
+view.displayHit('26');
+view.displayMessage('fuck Q man')
